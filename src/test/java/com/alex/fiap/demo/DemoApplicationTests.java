6931f76ed32ca7025d526145771ebba262c05e05
amouraorr <amouraorr@yahoo.com.br>
2024-11-08 12:44:44 -0300

    configurações

---
+++ src/test/java/com/alex/fiap/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.alex.fiap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
