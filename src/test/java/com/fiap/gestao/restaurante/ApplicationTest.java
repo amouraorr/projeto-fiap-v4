a97445c2e298b728eb5afdaceb4c65a9453a7ae0
Alexsandro <amouraorr@yahoo.com.br>
2025-02-20 09:06:32 -0300

    Impelemntação da cobertura de teste para a AddressService e metodos.

---
+++ src/test/java/com/fiap/gestao/restaurante/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.fiap.gestao.restaurante;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    @DisplayName("Deve carregar o contexto da aplicação corretamente")
+    void shouldLoadApplicationContext() {
+        assert true;
+    }
+}
